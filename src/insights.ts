41c2dbb5b77377b606ee11e544a0359f78dea567
vgensw <v.gensthaler@semmelweis.info>
2020-12-12 11:15:17 +0100

    Update and rename insights.js to insights.ts

---
+++ src/insights.ts
@@ -0,0 +1,18 @@
+import { Typeform } from './typeform-types'
+
+export class Insights {
+  constructor (private _http: Typeform.HTTPClient) { }
+  
+  public retrieve (
+    args: {
+      uid: string
+    }
+  ): Promise<Typeform.API.Insights.Retrieve> {
+    const { uid } = args
+
+    return this._http.request({
+      method: 'get',
+      url: `/insights/${uid}/summary`,
+    })
+  }
+}
